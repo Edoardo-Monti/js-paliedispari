259683fb4c305e3bad3b750e7c9e638f917f393d
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-30 15:53:45 +0200

    creazione delle funzioni

--- pari e dispari/assets/main.js
+++ pari e dispari/assets/main.js
@@ -3,6 +3,7 @@ Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
@@ -12,16 +13,28 @@ const buttonPari = document.querySelector(`.cursor1`)
 const buttonDispari = document.querySelector(`.cursor2`)
 
 buttonPari.addEventListener(`click`, function(){
-    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 5`))
+    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 6`))
     console.log(tuoNumero)
 })
 
 buttonDispari.addEventListener(`click`, function(){
-    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 5`))
+    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 6`))
     console.log(tuoNumero)
 })
 
-let numeroPc =  parseInt(Math.floor(Math.random()*6)+1)
+//genero un numero random da 1 a 6 con una funzione
+function numeroRandom(){
+    let numeroPc =  parseInt(Math.floor(Math.random()*6)+1)
     console.log(numeroPc)
+    return numeroPc
+}
+//richiamo la funzione, se no non vedo il risultato
+numeroRandom();
+
+function sommaNumeri(num1, num2){
+    console.log(num1 + num2)
+}
+
+sommaNumeri(tuoNumero, numeroRandom())
 
 
