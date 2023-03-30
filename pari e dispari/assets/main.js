719031af3baad084dc32e55437b6e30ca70f452e
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-30 15:30:40 +0200

    svolgimeto es. pari/dispari

--- pari e dispari/assets/main.js
+++ pari e dispari/assets/main.js
@@ -1,8 +1,27 @@
 /*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+const buttonPari = document.querySelector(`.cursor1`)
+const buttonDispari = document.querySelector(`.cursor2`)
+
+buttonPari.addEventListener(`click`, function(){
+    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 5`))
+    console.log(tuoNumero)
+})
+
+buttonDispari.addEventListener(`click`, function(){
+    let tuoNumero = parseInt(prompt(`scrivi un numero da 1 a 5`))
+    console.log(tuoNumero)
+})
+
+let numeroPc =  parseInt(Math.floor(Math.random()*6)+1)
+    console.log(numeroPc)
+
+
